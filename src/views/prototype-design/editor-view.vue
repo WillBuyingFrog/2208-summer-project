4df4c4439e24a30af1f64ffdfa8820614e41bc90
Will Frog <froggywch@gmail.com>
2022-08-02 13:32:17 +0800

    Initial work for drag-and-drop support

---
+++ src/views/prototype-design/editor-view.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: "editor-view",
+  props:{
+    value: Array,
+    parentId: String,
+    parent: Object
+  },
+  provide(){
+    return {
+      // 在子组件中使用inject: ['editorDOM']
+      editorDOM: this.$refs.editor
+    }
+  },
+  methods: {
+
+  },
+
+  render(){
+    return (
+        <div class={`ds-editor ${this.parentId ? 'nest-editor' : 'root-editor'}`}>
+          <div class="ds-editor-canvas" ref="editor">
+            {
+              // TODO: 抄CellWrapperVue
+            }
+          </div>
+        </div>
+    )
+  }
+}
+</script>
+
+<style scoped>
+.ds-editor{
+  color: black;
+}
+</style>
---
+++ src/views/prototype-design/plugins/plugin-drop.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  name: "plugin-drop",
+  render(){
+    return (
+        <div class="guide">
+          <div>拖拽组件或本地图片到此区域开始编辑</div>
+          <div>Drag component or local pictures to this area to start editing</div>
+        </div>
+    )
+  }
+}
+</script>
+
+<style scoped>
+.guide {
+  left: 50%;
+  top: 50%;
+  position: absolute;
+  transform: translate(-50%, -50%);
+  color: #999;
+  line-height: 2;
+  font-size: 16px;
+  font-weight: 300;
+}
+</style>
