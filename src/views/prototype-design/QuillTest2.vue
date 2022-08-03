5dd17f4b344c7b965b9d3670380892a20a24074e
Will Frog <froggywch@gmail.com>
2022-08-03 20:45:44 +0800

    Save current work

---
+++ src/views/prototype-design/QuillTest2.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="editor">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "QuillTest2",
+  data(){
+    return {
+      idReady: false,
+      quill: null
+    }
+  },
+  mounted(){
+    this.$data.quill
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
