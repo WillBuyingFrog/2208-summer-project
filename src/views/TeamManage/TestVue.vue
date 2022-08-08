8302a97917b6758ad3a5fb707bc5d51b2540a3db
Gatlin1111 <deng.haoyue@outlook.com>
2022-08-08 13:33:08 +0800

    image.js

--- src/views/TeamManage/TestVue.vue
+++ src/views/TeamManage/TestVue.vue
@@ -1,5 +1,5 @@
 <template>
-    <el-container>
+    <el-container >
       <el-header style="background: rgb(240,240,240);">
         <el-row >
             <el-col :span="2">
@@ -9,16 +9,17 @@
                 <h2>原型设计名字</h2>
             </el-col>
             <el-col :span="4">
-                <el-button>全屏预览</el-button>
+                <el-button @click="toggleFullscreen">全屏预览</el-button>
             </el-col>
         </el-row>
       </el-header>
       <el-container>
         <el-aside width="300px" style="background: rgb(240,240,240);height:100vh;">
-            <h3>页面（{{list.length}}）</h3>
+            <h4>页面（{{list.length}}）</h4>
             <el-divider></el-divider>
             <el-table :data="list" style="width: 100%;background-color:transparent;" 
-                :show-header="false"  row-style="background:transparent; height:50px">
+                :show-header="false"  row-style="background:transparent; height:50px"
+                @row-click = change highlight-current-row="true">
                 <el-table-column>
                     <template #default="scope">
                        <el-icon><CopyDocument /></el-icon>
@@ -28,19 +29,23 @@
             </el-table>
         </el-aside>
         <el-main>
-            <el-image :src="list[0].url" style="height:630px;"></el-image>
+            <el-image :src="src" style="height:630px;" id="img"></el-image>
         </el-main>
       </el-container>
     </el-container>
+    <!-- <el-container >
+        <el-image :src="src" style="height:100%;"></el-image>
+    </el-container> -->
 </template>
 
 <script>
+import screenfull from 'screenfull'
 export default{
     data(){
         return{
             list:[{
                 name:'首页',
-                url:'https://img2.baidu.com/it/u=1792249350,650626052&fm=253&fmt=auto&app=120&f=JPEG?w=1200&h=675',
+                url:'blob:http://localhost:8080/f906ec4e-5afd-4223-b62c-6103232836f5',
             },{
                 name:'登录、注册页',
                 url:'https://img2.baidu.com/it/u=2841427495,874700757&fm=253&fmt=auto&app=120&f=GIF?w=1017&h=800',
@@ -53,19 +58,23 @@ export default{
             },{
                 name:'个人信息',
                 url:'https://img2.baidu.com/it/u=1061245231,4112198693&fm=253&fmt=auto&app=138&f=JPEG?w=438&h=340',
-            }]
+            }],
+            src:'blob:http://localhost:8080/f906ec4e-5afd-4223-b62c-6103232836f5',
         }
     },
-    // methods: {
-    //     show(row){
-
-    //     }
-    // },
-    // computed:{
-    //     picture(){
-
-    //     }
-    // }
+    methods: {
+        change(row){
+           this.src = row.url;
+        },
+        toggleFullscreen() {
+            if (screenfull.isEnabled) {
+                const ele = document.getElementById('img');
+                screenfull.toggle(ele)
+                
+            }
+        }
+    },
+   
 }
 
 </script>
